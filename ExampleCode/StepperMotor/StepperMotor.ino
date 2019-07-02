b91e82a0c41b4edb243bad034cff44a63d01fb41
zba24 <zba24@uclive.ac.nz>
2019-07-02 13:50:12 +1200

    Added SW parts

--- ExampleCode/StepperMotor/StepperMotor.ino
+++ ExampleCode/StepperMotor/StepperMotor.ino
@@ -2,15 +2,8 @@
 This sample code is for testing the 4 stepper motors
 All motors will rotate the same direction at the same time
 */
-int M1dirpin = 39;
-int M1steppin = 38;
-int M2dirpin = 41;
-int M2steppin = 40;
-
-int M3dirpin = 45;
-int M3steppin = 44;
-int M4dirpin = 43;
-int M4steppin = 42;
+int M1dirpin = 20;
+int M1steppin = 21;
 
 void setup()
 {
@@ -20,15 +13,6 @@ void setup()
   //Setup step and direction pins for output
   pinMode(M1dirpin,OUTPUT);
   pinMode(M1steppin,OUTPUT);
-  
-  pinMode(M2dirpin,OUTPUT);
-  pinMode(M2steppin,OUTPUT);
-  
-  pinMode(M3dirpin,OUTPUT);
-  pinMode(M3steppin,OUTPUT);
-  
-  pinMode(M4dirpin,OUTPUT);
-  pinMode(M4steppin,OUTPUT);
 }
 
 void loop()
@@ -37,21 +21,12 @@ void loop()
   
   //Set direction for all channels
   digitalWrite(M1dirpin,LOW);
-  digitalWrite(M2dirpin,LOW);
-  digitalWrite(M3dirpin,LOW);
-  digitalWrite(M4dirpin,LOW);
   
   for(j=0;j<=1000;j++)            //Move 1000 steps
   {
     digitalWrite(M1steppin,LOW);
-    digitalWrite(M2steppin,LOW);
-    digitalWrite(M3steppin,LOW);
-    digitalWrite(M4steppin,LOW);
     delayMicroseconds(2);
     digitalWrite(M1steppin,HIGH);
-    digitalWrite(M2steppin,HIGH);
-    digitalWrite(M3steppin,HIGH);
-    digitalWrite(M4steppin,HIGH);
     delay(1);
   }
 }
